c22a78509803c793d3dcbeb81e3e13135d47aa4f
Alan Shaji <alanshaji1000@gmail.com>
2023-10-23 09:15:23 +0530

    switched to mysql

--- ecommerceproject/ecommerceproject/settings.py
+++ ecommerceproject/ecommerceproject/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'ecommerceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerceproject',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
